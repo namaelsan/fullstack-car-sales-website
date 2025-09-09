c8748d759a59f26bd51d0359a5e91b4544496d56
enhar.apuhan <enharapuhan@outlook.com>
2025-09-09 13:45:41 +0300

    filter added to frontend. incomplete feature

--- frontend/carCommerceApp/src/app/top-bar/top-bar.component.ts
+++ frontend/carCommerceApp/src/app/top-bar/top-bar.component.ts
@@ -1,19 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { SearchModel } from '../search-model.models';
+import { CarSearchCriteria } from '../car-search-criteria.models';
+import { Brand } from '../car.models';
+import { BrandService } from '../brand.service';
+import { map, Observable } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule, MatDateRangeInput } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { SearchService } from '../search-service.service';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, MatRadioModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit{
 
-public searchQuery: string = "";
+  maxPrice: number = 0;
+  minPrice: number = 0;
+  brands: Brand[] = [];
+  used: boolean = true;
+  startDate: Date = new Date(); 
+  endDate: Date = new Date();
+  minLitre: number = 0;
+  maxLitre: number = 0;
+
+  constructor (private brandService: BrandService, private searchService: SearchService) {}
+
+  public searchModel: SearchModel<CarSearchCriteria> = {
+    searchRequest: {
+      pageSize: 10,
+      pageIndex: 0,
+      sortDir: "ASC",
+      sortName: "id"
+    },
+    model: {
+      // brand: "",
+      // specification: "",
+      // litre: [0,1000],
+      // used: true,
+      // price: [0,1000],
+      // releaseDateTime: [new Date('2024-09-08T12:35:12.920+00:00'),new Date('2026-09-08T12:35:12.920+00:00')]
+    }
+  }
 
   search() {
+    this.searchService.searchCars(this.searchModel)
+  }
+
+  getAllBrands(): Observable<Brand[]> {
+  
+    return this.brandService.getAllBrands();
+  }
+
+
+  ngOnInit(): void {
+    this.getAllBrands().subscribe({
+      next: (brands) => {
+        this.brands = brands;
+      },
+      error: (e) => {
+        console.error("Error gettings brands", e)
+        this.brands = [];
+      }
+    });
     
   }
 }
